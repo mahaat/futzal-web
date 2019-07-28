1dbfe2abd3d6159d080ed7e820d0bee4bebfd6df
aditm <aditm@gitlab.lab>
2019-07-28 16:40:04 +0700

    update tanbah login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { LandingContainerComponent } from "./features/landing/landing-container/landing-container.component";
-
-const routes: Routes = [{ path: "", component: LandingContainerComponent }];
+const routes: Routes = [
+  {
+    path: "",
+    children: [
+      {
+        path: "",
+        component: LandingContainerComponent
+      },
+      {
+        path: "auth",
+        loadChildren: "./features/auth/auth.module#AuthModule"
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
